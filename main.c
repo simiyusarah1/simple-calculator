baf6b60690ced3c03c84b151381f2447dac22393
simiyusarah1 <106378785+simiyusarah1@users.noreply.github.com>
2022-07-01 11:11:11 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+simple calculator
+created by Abonyo Sarah
+on July1,2022
+c89 complier
+MIT license
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\simple calculator\n");
+    return 0;
+}
